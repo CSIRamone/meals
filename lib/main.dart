0aa52443be3982bab6909f74ce86a9cd2909797b
CSIRamone <cidinei_inacio@hotmail.com>
2023-07-19 23:33:28 -0300

    Primeiro commit - models, tela categoria, componente item categoria, tema e estilo,Navegando para uma nova tela Navigator.of(context).push()

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'screens/category_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        hintColor: Colors.amber,
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
